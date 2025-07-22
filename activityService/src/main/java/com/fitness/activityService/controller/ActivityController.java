9f6be26d8b302d8def77837e9aca367561dc4f7e
rahulj1011 <rahuljr2022@gmail.com>
2025-07-23 00:27:46 +0530

    added activity service

--- activityService/src/main/java/com/fitness/activityService/controller/ActivityController.java
+++ activityService/src/main/java/com/fitness/activityService/controller/ActivityController.java
@@ -1,22 +1,38 @@
 package com.fitness.activityService.controller;
 
 
+import com.fitness.activityService.dto.ActivityRequest;
 import com.fitness.activityService.dto.ActivityResponse;
+import com.fitness.activityService.service.ActivityService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/activities")
 public class ActivityController
 {
 
-    private  ActivityService activityservice;
+    private ActivityService activityservice;
+
+    @PostMapping
     public ResponseEntity<ActivityResponse> trackActivity(@RequestBody ActivityRequest request)
     {
         return ResponseEntity.ok(activityservice.trackActivity(request));
+    }
+
+
+    @GetMapping
+    public ResponseEntity <List<ActivityResponse>> getUserActivity(@RequestHeader("X-User-ID") String UserID)
+    {
+        return ResponseEntity.ok(activityservice.getUseractivities(UserID));
+    }
+
+    @GetMapping("/{activityId}")
+    public ResponseEntity <ActivityResponse> getActivity(@PathVariable String activityId)
+    {
+        return ResponseEntity.ok(activityservice.getActivityById(activityId));
     }
 
 }
--- activityService/src/main/java/com/fitness/activityService/service/ActivityService.java
+++ activityService/src/main/java/com/fitness/activityService/service/ActivityService.java
@@ -8,6 +8,9 @@ import com.fitness.activityService.model.Activity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class ActivityService {
@@ -23,7 +26,43 @@ public class ActivityService {
                 .startTime(request.getStartTime())
                 .additionalMetrics(request.getAdditionalMetrics())
                 .build();
+        Activity savedActivity = activityRepository.save(activity);
+        return mapToResponse(savedActivity);
+    }
+
+
+    public ActivityResponse mapToResponse(Activity activity)
+    {
+        ActivityResponse response = new ActivityResponse();
+        response.setId(activity.getId());
+        response.setCreatedAt(activity.getCreatedAt());
+        response.setUpdatedAt(activity.getUpdatedAt());
+        response.setDuration(activity.getDuration());
+        response.setType(activity.getType());
+        response.setCaloriesBurned(activity.getCaloriesBurned());
+        response.setStartTime(activity.getStartTime());
+        response.setAdditionalMetrics(activity.getAdditionalMetrics());
+        return response;
+    }
+
+
+    public List<ActivityResponse>getUseractivities(String userId)
+    {
+       List<Activity>activities =  activityRepository.findByUserId(userId);
+
+        return activities.stream()
+                .map(this::mapToResponse)
+                .collect(Collectors.toList());
+    }
+
+
+    public ActivityResponse getActivityById(String activityId)
+    {
+        return activityRepository.findById(activityId)
+                .map(this::mapToResponse)
+                .orElseThrow(()-> new RuntimeException("Activity not used"));
     }
 }
+
 
 
